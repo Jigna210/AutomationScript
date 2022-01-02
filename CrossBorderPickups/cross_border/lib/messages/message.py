8eae59230627f256012fcf4fa22d41cf8625ffb8
Jigna210 <70285179+Jigna210@users.noreply.github.com>
2022-01-03 00:21:04 +0530

    Update message.py

--- CrossBorderPickups/cross_border/lib/messages/message.py
+++ CrossBorderPickups/cross_border/lib/messages/message.py
@@ -17,14 +17,3 @@ class NotificationMessages:
         invalid_email_error = "Email must be a valid email address"
         password_required = "Password is required"
         invalid_credentials = "No active account found with the given credentials"
-
-    class NewPackagePage:
-        """ Notifications messages from New Packages page """
-        create_package_success_msg = "Package created"
-        full_name_error_msg = "Account is required"
-        pkg_status_error_msg = "Status is required"
-        date_received_error_msg = "Received Date is required"
-        carrier_error_msg = "Carrier is required"
-        tracking_number_error_msg = "Tracking number is required"
-        vendor_error_msg = "Vendor is required"
-        weight_error_msg = "Weight is required"
